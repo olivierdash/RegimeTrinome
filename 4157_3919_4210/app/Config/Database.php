a8b4d912a2014ad49448ad62b4f30b218102f87e
olivierdash <otahiriniaina@gmail.com>
2026-05-11 01:12:09 +0300

    base de donnees

--- 4157_3919_4210/app/Config/Database.php
+++ 4157_3919_4210/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'regime_app',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
